3d1e9de3fec3cf79ab8fca123e3d385fa798821b
Vishal9610 <loveindiabyvishal9616@gmail.com>
2026-08-12 16:22:19 +0530

    Fix Render MySQL connection

--- config.py
+++ config.py
@@ -1,33 +1,12 @@
 import os
-from flask_mysqldb import MySQL
-
-mysql = MySQL()
-
 
 class Config:
 
-    MYSQL_HOST = os.environ.get(
-        "MYSQL_HOST",
-        "sql12.freesqldatabase.com"
-    )
-
-    MYSQL_USER = os.environ.get(
-        "MYSQL_USER",
-        "sql12834978"
-    )
-
-    MYSQL_PASSWORD = os.environ.get(
-        "MYSQL_PASSWORD"
-    )
-
-    MYSQL_DB = os.environ.get(
-        "MYSQL_DB",
-        "sql12834978"
-    )
-
-    MYSQL_PORT = int(
-        os.environ.get("MYSQL_PORT", "3306")
-    )
+    MYSQL_HOST = os.environ.get("MYSQL_HOST")
+    MYSQL_USER = os.environ.get("MYSQL_USER")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+    MYSQL_DB = os.environ.get("MYSQL_DB")
+    MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
 
     SECRET_KEY = os.environ.get(
         "SECRET_KEY",
