65b217e999ccf1a39e9c613bdcef3da9c49b78a8
Vishal9610 <loveindiabyvishal9616@gmail.com>
2026-08-19 01:28:54 +0530

    Update Bright Future NGO website

--- config.py
+++ config.py
@@ -120,6 +120,93 @@
 
 
 
+# import os
+
+# from flask_mysqldb import MySQL
+# from flask_mail import Mail
+
+# mysql = MySQL()
+# mail = Mail()
+
+
+# class Config:
+
+#     # =========================
+#     # Flask Session
+#     # =========================
+#     SECRET_KEY = os.environ.get(
+#         "SECRET_KEY",
+#         "brightfuture_local_secret_key_2026"
+#     )
+
+#     # =========================
+#     # MySQL - Render / FreeSQLDatabase
+#     # =========================
+#     MYSQL_HOST = os.environ.get(
+#         "MYSQL_HOST",
+#         "sql12.freesqldatabase.com"
+#     )
+
+#     MYSQL_USER = os.environ.get(
+#         "MYSQL_USER",
+#         "sql12835518"
+#     )
+
+#     MYSQL_PASSWORD = os.environ.get(
+#         "MYSQL_PASSWORD",
+#         "qiLhjnnbqK"
+#     )
+
+#     MYSQL_DB = os.environ.get(
+#         "MYSQL_DB",
+#         "sql12835518"
+#     )
+
+#     MYSQL_PORT = int(
+#         os.environ.get("MYSQL_PORT", "3306")
+#     )
+
+    
+# # =========================
+# # Cloudinary
+# # =========================
+# CLOUDINARY_CLOUD_NAME = os.environ.get( "CLOUDINARY_CLOUD_NAME","cypipohx")
+# CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY","884271135157468")
+# CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET","fAmH5p6FiM1F9wATItKtZ5WZxew")
+
+#     # =========================
+#     # Gallery Upload
+#     # =========================
+# UPLOAD_FOLDER = "static/uploads/gallery"
+
+#     # =========================
+#     # Gmail SMTP
+#     # =========================
+# MAIL_SERVER = "smtp.gmail.com"
+# MAIL_PORT = 587
+# MAIL_USE_TLS = True
+# MAIL_USE_SSL = False
+
+# MAIL_USERNAME = os.environ.get(
+#         "MAIL_USERNAME",
+#         "loveindiabyvishal9616@gmail.com"
+#     )
+
+# MAIL_PASSWORD = os.environ.get(
+#         "MAIL_PASSWORD",
+#         "xmrfcmwdavhkuddq"
+#     )
+
+# MAIL_DEFAULT_SENDER = os.environ.get(
+#         "MAIL_DEFAULT_SENDER",
+#         "loveindiabyvishal9616@gmail.com"
+#     )
+
+
+
+
+
+
 import os
 
 from flask_mysqldb import MySQL
@@ -140,7 +227,7 @@ class Config:
     )
 
     # =========================
-    # MySQL - Render / FreeSQLDatabase
+    # MySQL
     # =========================
     MYSQL_HOST = os.environ.get(
         "MYSQL_HOST",
@@ -166,39 +253,51 @@ class Config:
         os.environ.get("MYSQL_PORT", "3306")
     )
 
-    
-# =========================
-# Cloudinary
-# =========================
-CLOUDINARY_CLOUD_NAME = os.environ.get( "CLOUDINARY_CLOUD_NAME","cypipohx")
-CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY","884271135157468")
-CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET","fAmH5p6FiM1F9wATItKtZ5WZxew")
+    # =========================
+    # Cloudinary
+    # =========================
+    CLOUDINARY_CLOUD_NAME = os.environ.get(
+        "CLOUDINARY_CLOUD_NAME",
+        "cypipohx"
+    )
+
+    CLOUDINARY_API_KEY = os.environ.get(
+        "CLOUDINARY_API_KEY",
+        "884271135157468"
+    )
+
+    CLOUDINARY_API_SECRET = os.environ.get(
+        "CLOUDINARY_API_SECRET",
+        "fAmH5p6FiM1F9wATItKtZ5WZxew"
+    )
 
     # =========================
     # Gallery Upload
     # =========================
-UPLOAD_FOLDER = "static/uploads/gallery"
+    UPLOAD_FOLDER = "static/uploads/gallery"
 
     # =========================
     # Gmail SMTP
     # =========================
-MAIL_SERVER = "smtp.gmail.com"
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USE_SSL = False
+    MAIL_SERVER = "smtp.gmail.com"
 
-MAIL_USERNAME = os.environ.get(
+    MAIL_PORT = 587
+
+    MAIL_USE_TLS = True
+
+    MAIL_USE_SSL = False
+
+    MAIL_USERNAME = os.environ.get(
         "MAIL_USERNAME",
         "loveindiabyvishal9616@gmail.com"
     )
 
-MAIL_PASSWORD = os.environ.get(
+    MAIL_PASSWORD = os.environ.get(
         "MAIL_PASSWORD",
         "xmrfcmwdavhkuddq"
     )
 
-MAIL_DEFAULT_SENDER = os.environ.get(
+    MAIL_DEFAULT_SENDER = os.environ.get(
         "MAIL_DEFAULT_SENDER",
         "loveindiabyvishal9616@gmail.com"
     )
-
