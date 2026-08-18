5f62c4df275c977163dfaf2e7f7f965357c3fdaf
Vishal9610 <loveindiabyvishal9616@gmail.com>
2026-08-18 22:58:16 +0530

    Update Bright Future NGO website

--- config.py
+++ config.py
@@ -64,6 +64,62 @@
 
 
 
+# import os
+
+# from flask_mysqldb import MySQL
+# from flask_mail import Mail
+
+# mysql = MySQL()
+# mail = Mail()
+
+
+# class Config:
+
+#     # =========================
+#     # Flask Session
+#     # =========================
+#     SECRET_KEY = os.environ.get(
+#         "SECRET_KEY",
+#         "brightfuture_local_secret_key_2026"
+#     )
+
+#     # =========================
+#     # MySQL
+#     # =========================
+#     MYSQL_HOST = os.environ.get("sql12.freesqldatabase.com", "localhost")
+#     MYSQL_USER = os.environ.get("sql12835518", "root")
+#     MYSQL_PASSWORD = os.environ.get("qiLhjnnbqK", "")
+#     MYSQL_DB = os.environ.get("sql12835518", "brightfuture")
+#     MYSQL_PORT = int(os.environ.get("3306", "3306"))
+#     # Aiven MySQL SSL
+#     MYSQL_SSL_MODE = os.environ.get("REQUIRED", "REQUIRED")
+
+   
+
+#     # =========================
+#     # Gallery Upload
+#     # =========================
+#     UPLOAD_FOLDER = "static/uploads/gallery"
+
+#     # =========================
+#     # Gmail SMTP
+#     # =========================
+#     MAIL_SERVER = "smtp.gmail.com"
+#     MAIL_PORT = 587
+#     MAIL_USE_TLS = True
+#     MAIL_USE_SSL = False
+
+#     MAIL_USERNAME = os.environ.get("loveindiabyvishal9616@gmail.com")
+#     MAIL_PASSWORD = os.environ.get("xmrfcmwdavhkuddq")
+
+#     MAIL_DEFAULT_SENDER = os.environ.get("loveindiabyvishal9616@gmail.com")
+
+
+
+
+
+
+
 import os
 
 from flask_mysqldb import MySQL
@@ -84,13 +140,31 @@ class Config:
     )
 
     # =========================
-    # MySQL
+    # MySQL - Render / FreeSQLDatabase
     # =========================
-    MYSQL_HOST = os.environ.get("sql12.freesqldatabase.com", "localhost")
-    MYSQL_USER = os.environ.get("sql12835175", "root")
-    MYSQL_PASSWORD = os.environ.get("aLV9Cnn6MN", "")
-    MYSQL_DB = os.environ.get("sql12835175", "brightfuture")
-    MYSQL_PORT = int(os.environ.get("3306", "3306"))
+    MYSQL_HOST = os.environ.get(
+        "MYSQL_HOST",
+        "sql12.freesqldatabase.com"
+    )
+
+    MYSQL_USER = os.environ.get(
+        "MYSQL_USER",
+        "sql12835518"
+    )
+
+    MYSQL_PASSWORD = os.environ.get(
+        "MYSQL_PASSWORD",
+        "qiLhjnnbqK"
+    )
+
+    MYSQL_DB = os.environ.get(
+        "MYSQL_DB",
+        "sql12835518"
+    )
+
+    MYSQL_PORT = int(
+        os.environ.get("MYSQL_PORT", "3306")
+    )
 
     # =========================
     # Gallery Upload
@@ -105,7 +179,17 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
 
-    MAIL_USERNAME = os.environ.get("loveindiabyvishal9616@gmail.com")
-    MAIL_PASSWORD = os.environ.get("xmrfcmwdavhkuddq")
+    MAIL_USERNAME = os.environ.get(
+        "MAIL_USERNAME",
+        "loveindiabyvishal9616@gmail.com"
+    )
 
-    MAIL_DEFAULT_SENDER = os.environ.get("loveindiabyvishal9616@gmail.com")
+    MAIL_PASSWORD = os.environ.get(
+        "MAIL_PASSWORD",
+        "xmrfcmwdavhkuddq"
+    )
+
+    MAIL_DEFAULT_SENDER = os.environ.get(
+        "MAIL_DEFAULT_SENDER",
+        "loveindiabyvishal9616@gmail.com"
+    )
