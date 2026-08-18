6e598769398fc9b396907af93046b6a842c89e2c
Vishal9610 <loveindiabyvishal9616@gmail.com>
2026-08-18 15:13:11 +0530

    Update Bright Future NGO website

--- config.py
+++ config.py
@@ -17,55 +17,6 @@
 
 
 
-from flask_mysqldb import MySQL
-from flask_mail import Mail
-
-mysql = MySQL()
-mail = Mail()
-
-
-class Config:
-
-    # Flask Session
-    SECRET_KEY = "brightfuture_local_secret_key_2026"
-
-    # MySQL
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = ""
-    MYSQL_DB = "brightfuture"
-
-    # Gallery Upload
-    UPLOAD_FOLDER = "static/uploads/gallery"
-
-    # Gmail SMTP
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-
-    # IMPORTANT:
-    # Apna real Gmail yahan likhein
-    MAIL_USERNAME = "loveindiabyvishal9616@gmail.com"
-
-    # Gmail ka 16-digit APP PASSWORD yahan likhein
-    MAIL_PASSWORD = "kliekvxqfynksxvv"
-
-    MAIL_DEFAULT_SENDER = "loveindiabyvishal9616@gmail.com"
-
-
-
-
-
-
-
-
-
-
-
-
-# import os
-
 # from flask_mysqldb import MySQL
 # from flask_mail import Mail
 
@@ -75,37 +26,86 @@ class Config:
 
 # class Config:
 
-#     # =========================
 #     # Flask Session
-#     # =========================
-#     SECRET_KEY = os.environ.get(
-#         "SECRET_KEY",
-#         "brightfuture_local_secret_key_2026"
-#     )
+#     SECRET_KEY = "brightfuture_local_secret_key_2026"
 
-#     # =========================
 #     # MySQL
-#     # =========================
-#     MYSQL_HOST = os.environ.get("sql12.freesqldatabase.com", "localhost")
-#     MYSQL_USER = os.environ.get("sql12835175", "root")
-#     MYSQL_PASSWORD = os.environ.get("aLV9Cnn6MN", "")
-#     MYSQL_DB = os.environ.get("sql12835175", "brightfuture")
-#     MYSQL_PORT = int(os.environ.get("3306", "3306"))
+#     MYSQL_HOST = "localhost"
+#     MYSQL_USER = "root"
+#     MYSQL_PASSWORD = ""
+#     MYSQL_DB = "brightfuture"
 
-#     # =========================
 #     # Gallery Upload
-#     # =========================
 #     UPLOAD_FOLDER = "static/uploads/gallery"
 
-#     # =========================
 #     # Gmail SMTP
-#     # =========================
 #     MAIL_SERVER = "smtp.gmail.com"
 #     MAIL_PORT = 587
 #     MAIL_USE_TLS = True
 #     MAIL_USE_SSL = False
 
-#     MAIL_USERNAME = os.environ.get("loveindiabyvishal9616@gmail.com")
-#     MAIL_PASSWORD = os.environ.get("kliekvxqfynksxvv")
+#     # IMPORTANT:
+#     # Apna real Gmail yahan likhein
+#     MAIL_USERNAME = "loveindiabyvishal9616@gmail.com"
 
-#     MAIL_DEFAULT_SENDER = os.environ.get("loveindiabyvishal9616@gmail.com")
+#     # Gmail ka 16-digit APP PASSWORD yahan likhein
+#     MAIL_PASSWORD = "xmrfcmwdavhkuddq"
+
+#     MAIL_DEFAULT_SENDER = "loveindiabyvishal9616@gmail.com"
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+
+from flask_mysqldb import MySQL
+from flask_mail import Mail
+
+mysql = MySQL()
+mail = Mail()
+
+
+class Config:
+
+    # =========================
+    # Flask Session
+    # =========================
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "brightfuture_local_secret_key_2026"
+    )
+
+    # =========================
+    # MySQL
+    # =========================
+    MYSQL_HOST = os.environ.get("sql12.freesqldatabase.com", "localhost")
+    MYSQL_USER = os.environ.get("sql12835175", "root")
+    MYSQL_PASSWORD = os.environ.get("aLV9Cnn6MN", "")
+    MYSQL_DB = os.environ.get("sql12835175", "brightfuture")
+    MYSQL_PORT = int(os.environ.get("3306", "3306"))
+
+    # =========================
+    # Gallery Upload
+    # =========================
+    UPLOAD_FOLDER = "static/uploads/gallery"
+
+    # =========================
+    # Gmail SMTP
+    # =========================
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+
+    MAIL_USERNAME = os.environ.get("loveindiabyvishal9616@gmail.com")
+    MAIL_PASSWORD = os.environ.get("xmrfcmwdavhkuddq")
+
+    MAIL_DEFAULT_SENDER = os.environ.get("loveindiabyvishal9616@gmail.com")
